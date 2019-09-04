5bdf665c57bdcd7b623dc1c4760ab55c98c9ec3a
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-04 00:14:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(favorite_song,favorite_artist)
   favorite_song.concat(favorite_artist)
 end
 
-def using_insert(array,)
+def using_insert(array,element)
   a1 = [1,2,3]
   print a1.insert(2,6)
   puts
